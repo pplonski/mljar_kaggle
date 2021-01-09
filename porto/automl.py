cad8fd94389389a3f04a6e0d221c84c98c83aab1
Piotrek <pplonski86@gmail.com>
2021-01-09 21:47:55 +0100

    add porto

--- porto/automl.py
+++ porto/automl.py
@@ -6,7 +6,7 @@ test  = pd.read_csv("test.csv")
 sub = pd.read_csv("sample_submission.csv")
 x_cols = train.columns[2:]
 
-automl = AutoML(mode="Compete", total_time_limit=12) #4*3600)
+automl = AutoML(mode="Compete", total_time_limit=4*3600)
 automl.fit(train[x_cols], train["target"])
 
 sub["target"] = automl.predict_proba(test)[:,1]
